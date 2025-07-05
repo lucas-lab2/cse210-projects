e36a480008c361c737ab14721a0b13f776a0e09a
Lucas Felipe <lucas.miranda880@outlook.com>
2025-07-05 14:14:35 -0300

    exercise 3 commpleted both the core and strech requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        do
+        {
+            int MagicNumber = 7;
+            int GuessCount = 0;
+
+            Console.WriteLine("Hello Player! This is the Guess My Number game!");
+
+            Console.WriteLine($"What is the Magic Number? {MagicNumber} ");
+
+            Console.WriteLine("What is your guess?");
+            int userNumber = int.Parse(Console.ReadLine());
+            GuessCount++;
+
+            while (userNumber != MagicNumber)
+            {
+                if (userNumber < MagicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userNumber > MagicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                GuessCount++;
+                Console.WriteLine("What is your guess?");
+                userNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Congratulations! You guessed the Magic Number {MagicNumber} in {GuessCount} tries!");
+            Console.WriteLine("Would you like to play again? (Y/N)");
+            response = Console.ReadLine().ToUpper();
+
+            while (response != "Y" && response != "N")
+            {
+                Console.WriteLine("Invalid input. Please enter Y or N.");
+                response = Console.ReadLine().ToUpper();
+            }
+        } while (response == "Y");
     }
 }
