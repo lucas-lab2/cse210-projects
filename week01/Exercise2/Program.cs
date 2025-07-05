683ea28aeb7fcbe2797450ba8030fd6c15ef36be
Lucas Felipe <lucas.miranda880@outlook.com>
2025-07-04 22:01:26 -0300

    forgot to comit the exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please, type the grade percentage you received in the course:");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("You received an A.");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("You received a B.");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You received a C.");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You received a D.");
+        }
+        else
+        {
+            Console.WriteLine("You received an F.");
+        }
     }
 }
