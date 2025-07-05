d2eea979bb6347d1522e9fef25f817b78d6d8616
Lucas Felipe <lucas.miranda880@outlook.com>
2025-07-05 15:03:28 -0300

    completed the exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int userNumber;
+        int soma = 0;
+        do
+        {
+            Console.WriteLine("Please enter a number (0 to stop):");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+                Console.WriteLine($"You entered: {userNumber}");
+            }
+
+        } while (userNumber != 0);
+            foreach (int number in numbers)
+            {
+                soma += number;
+            }
+            Console.WriteLine($"The sum of the numbers entered is: {soma}");
     }
 }
