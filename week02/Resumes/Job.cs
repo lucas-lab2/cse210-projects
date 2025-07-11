a68d339d2465682965961244e4c47b90009687ed
Lucas Felipe <lucas.miranda880@outlook.com>
2025-07-10 23:14:48 -0300

    modified the program.cs file to contain the paramenters that are being passed to my Job class. I also created the Job class within the JOb.cs file.

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        // Cria o objeto job1
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Solutions";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Data Insights";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+        
+
     }
 }
