e25b3fabdcbc1f9cc492bb98e271b20dfbeebd61
Lucas Felipe <lucas.miranda880@outlook.com>
2025-07-11 12:23:02 -0300

    started building the Program.cs file. This is where my program start and where the user will see the main menu. This file is the main lobby of my app. Created a loop that will display the menu and wait for the user to pick an option

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        bool keepRunning = true; // Variable to control the loop
+        while (keepRunning)
+        {
+            Console.WriteLine("Welcome to the Journal Project.");
+            Console.WriteLine("Please Select one of the following choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display all entries");
+            Console.WriteLine("3. Save entries to a file");
+            Console.WriteLine("4. Save the Journal to a file");
+
+            string choice = Console.ReadLine(); // Read user input
+
+
+            // Process the user's choice
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("You chose to write a new entry.");
+                    break;
+                case "2":
+                    Console.WriteLine("You chose to display the Journal.");
+                    break;
+                case "3":
+                    Console.WriteLine("You chose to load the Journal.");
+                    break;
+                case "4":
+                    Console.WriteLine("You chose to save the JOurnal to a file.");
+                    break;
+                case "5":
+                    keepRunning = false; // Exit the loop
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+
+            }
+
+
+        }
+        Console.WriteLine("Thank you for using the Journal Project. Goodbye!");
     }
 }
