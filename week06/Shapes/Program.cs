37c1836194c7216d56c6a2be84c6c3fc8505695b
Lucas Felipe <lucas.miranda880@outlook.com>
2025-08-04 12:04:40 -0300

    week 6 assginment completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List shapes = new List();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area:F2}");
+        }
     }
 }
